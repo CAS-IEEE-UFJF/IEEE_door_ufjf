172ca8ea1b2bed4c7c074f8777bf0bd9ac5de148
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2018-12-08 22:26:04 -0200

    leitura

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#[3º Versão](https://github.com/wesley-cantarino/IEEE_door_ufjf/tree/master/version3)
+# [3º Versão](https://github.com/wesley-cantarino/IEEE_door_ufjf/tree/master/version3)
 
 # [ 2º Versão](https://github.com/wesley-cantarino/IEEE_door_ufjf/tree/master/version2)
 Objetivo da 2º versão é apresentar para a equipe o nodeMCU (ESP8266) e desenvolver a capacidade de programação em IOT.
--- version3/exemplos/matrix_numerica/matrix_numerica.ino
+++ version3/exemplos/matrix_numerica/matrix_numerica.ino
@@ -1,4 +1,91 @@
-//https://techtutorialsx.com/2017/03/18/esp8266-interfacing-with-a-4x4-matrix-keypad/ (modificado 08/12/2018)
+//fazendo leitura na marra
+//ainda em desenvolvimento
+//parece que algumas teclas agarram
+
+#include <Arduino.h>
+
+const int l1 = 0;
+const int l2 = 12;
+const int l3 = 14;
+const int l4 = 2;
+
+const int c1 = 16;
+const int c2 = 4;
+const int c3 = 5;
+const int c4 = 13;
+
+void desligar (){
+  digitalWrite(l1, LOW);
+  digitalWrite(l2, LOW);
+  digitalWrite(l3, LOW);
+  digitalWrite(l4, LOW);
+}
+
+void pesquisar (){
+  if (digitalRead(c1) == HIGH){
+    Serial.println("c1");
+  }
+
+  if (digitalRead(c2) == HIGH){
+    Serial.println("c2");
+  }
+
+  if (digitalRead(c3) == HIGH){
+    Serial.println("c3");
+  }
+
+  if (digitalRead(c4) == HIGH){
+    Serial.println("c4");
+  }
+}
+
+void setup (){
+  Serial.begin(115200);
+
+  //Pinos ligados aos pinos 1, 2, 3 e 4 do teclado - Linhas
+  pinMode(l1, OUTPUT);
+  pinMode(l2, OUTPUT);
+  pinMode(l3, OUTPUT);
+  pinMode(l4, OUTPUT);
+
+  //Pinos ligados aos pinos 5, 6, 7 e 8 do teclado - Colunas
+  pinMode(c1, INPUT);
+  pinMode(c2, INPUT);
+  pinMode(c3, INPUT);
+  pinMode(c4, INPUT);
+
+  Serial.begin(115200);
+  Serial.println("Aguardando acionamento das teclas...");
+}
+
+void loop (){
+  desligar();
+  digitalWrite(l1, HIGH);
+  pesquisar();
+
+  delay(50);
+
+  desligar();
+  digitalWrite(l2, HIGH);
+  pesquisar();
+
+  delay(50);
+
+  desligar();
+  digitalWrite(l3, HIGH);
+  pesquisar();
+
+  delay(50);
+
+  desligar();
+  digitalWrite(l4, HIGH);
+  pesquisar();
+
+  delay(100);
+}
+
+//usando a biblioteca keypad :D
+/*//https://techtutorialsx.com/2017/03/18/esp8266-interfacing-with-a-4x4-matrix-keypad/ (modificado 08/12/2018)
 
 #include <Arduino.h>
 #include <Keypad.h>
@@ -30,3 +117,4 @@ void loop(){
     Serial.println(myKey);
   }
 }
+*/
